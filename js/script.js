215c05de6fc72eb691cc6fd5ce7f1a053b5e7ef8
natagetup <n.kulikouskaya@gmail.com>
2024-01-07 20:44:12 +0100

    feat: implement initial game render && clean up the body

--- js/script.js
+++ js/script.js
@@ -22,14 +22,13 @@ const getRandomQuestion = () => {
 };
 
 let guessesCounter = 0;
-let currentQuestion = getRandomQuestion();
+let currentQuestion;
 
 const renderVirtualKeyboard = () => {
   const alphabet = "abcdefghijklmnopqrstuvwxyz";
   const virtualKeyboard = document.createElement("div");
   virtualKeyboard.className = "virtual-keyboard";
   for (let letter of alphabet) {
-    console.log(letter);
     const button = document.createElement("button");
     button.className = "key-btn";
     button.innerHTML = letter.toUpperCase();
@@ -38,5 +37,65 @@ const renderVirtualKeyboard = () => {
   }
   return virtualKeyboard;
 };
+
+const initialRender = () => {
+  document.body.innerHTML = "";
+  const virtualKeyboard = renderVirtualKeyboard();
+  const question = currentQuestion = getRandomQuestion();
+  const answerLength = question.answer.length;
+  document.body.innerHTML = `<div class="main-container">
+    <header>
+      <h1 class="heading">Hangman Game</h1>
+    </header>
+      <div class="game-container">
+        <div class="gallows-container">
+          <img src="./assets/0.png" alt="" />
+        </div>
+        <div class="quiz">
+          <p class="answer"></p>
+          <p class="question"></p>
+          <p class="guesses">
+          Incorrect guesses:
+          <span class="guessess-counter" id="guessess-counter"></span>/6
+          </p>
+        </div>
+      </div>
+    </div>`;
+    const qizElement = document.querySelector('.quiz');
+    console.log(qizElement);
+    qizElement.append(virtualKeyboard);
+    answerPlaceholderRender(answerLength);
+    questionRender(question.question);
+    guessesRender(0);
+    gallowsRender(0);
+};
+
+const answerPlaceholderRender = (answerLength) => {
+  const answerElement = document.querySelector('.answer');
+  placeholder = '';
+
+  while(answerLength){
+    placeholder += '_';
+    answerLength--;
+  }
+  answerElement.innerHTML = placeholder;
+}
+
+const questionRender = (question) => {
+  const questionElement = document.querySelector('.question');
+  questionElement.innerHTML = question;
+}
+
+const guessesRender = (guessesCounter) => {
+  const questionElement = document.querySelector('.guessess-counter');
+  questionElement.innerHTML = `${guessesCounter}`;
+}
+
+const gallowsRender = (guessesCounter) => {
+  const gallowsImg = document.querySelector('.gallows-container img');
+  gallowsImg.src = `./assets/${guessesCounter}.png`;
+}
+
+initialRender();
 
 const handleKeyPress = (letter) => {};
