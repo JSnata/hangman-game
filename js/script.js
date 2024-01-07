f0672299f04fd25eac88ae46151a518c5b9e783c
natagetup <n.kulikouskaya@gmail.com>
2024-01-07 19:08:24 +0100

    feat: add styling && basic html structure && change project images

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const questionsData = [
+  { question: 'What is the capital of France?', answer: 'Paris' },
+  { question: 'Which planet is known as the Red Planet?', answer: 'Mars' },
+  { question: 'What color is the sky?', answer: 'Blue' },
+  { question: 'Which animal says "meow"?', answer: 'Cat' },
+  { question: 'What season comes after winter?', answer: 'Spring' },
+  { question: 'What is the name of the big, round thing in the sky at night?', answer: 'Moon' },
+  { question: 'How many legs does a cat have?', answer: 'Four' },
+  { question: 'What is the opposite of "hot"?', answer: 'Cold' },
+  { question: 'What do you wear on your feet?', answer: 'Shoes' },
+  { question: 'What is the opposite of "up"?', answer: 'Down' },
+  { question: 'What day comes after Sunday?', answer: 'Monday' }
+];
+
