28d1086e358c8189b54a3c04b53f59caf18052ea
natagetup <n.kulikouskaya@gmail.com>
2024-01-08 12:08:37 +0100

    feat: add keyHandler which changes the answer placeholder, gallows image and guesses counter

--- js/script.js
+++ js/script.js
@@ -22,7 +22,6 @@ let answerContainer;
 let questionContainer;
 let guessCounterContainer;
 
-
 const getRandomQuestion = () => {
   const randomIndex = Math.floor(Math.random() * questionsData.length);
   return questionsData[randomIndex];
@@ -30,51 +29,66 @@ const getRandomQuestion = () => {
 
 let guessesCounter = 0;
 let currentQuestion;
-
+let currentAnswerPlaceholder = "";
 
 const renderVirtualKeyboard = () => {
   const virtualKeyboard = renderElement("div", "virtual-keyboard", null);
   for (let letter of alphabet) {
-    const button = renderElement("button", "key-btn", virtualKeyboard, {innerHTML: letter.toUpperCase()})
+    const button = renderElement("button", "key-btn", virtualKeyboard, {
+      innerHTML: letter.toUpperCase(),
+    });
     button.addEventListener("click", () => handleKeyPress(letter));
   }
   return virtualKeyboard;
 };
 
-
 const renderElement = (child, className, parent, attr) => {
   const element = document.createElement(child);
   className && (element.className = className);
-  parent && (parent.append(element));
-  if(attr) {
-    for (let key in attr){
-    element[key] = attr[key];
+  parent && parent.append(element);
+  if (attr) {
+    for (let key in attr) {
+      element[key] = attr[key];
     }
   }
   return element;
-}
+};
 
 const initialRender = () => {
   document.body.innerHTML = "";
   const virtualKeyboard = renderVirtualKeyboard();
-  const question = currentQuestion = getRandomQuestion();
-  const answerLength = question.answer.length;
+  const question = (currentQuestion = getRandomQuestion());
   const mainContainer = renderElement("div", "main-container", document.body);
   const mainHeader = renderElement("header", null, mainContainer);
-  const mainHeading = renderElement("h1", "heading", mainHeader, {innerText: "Hangman Game"});
+  const mainHeading = renderElement("h1", "heading", mainHeader, {
+    innerText: "Hangman Game",
+  });
   const gameContainer = renderElement("div", "game-container", mainContainer);
-  const gallowsContainer = renderElement("div", "gallows-container", gameContainer);
-  gallowsImg = renderElement("img", null, gallowsContainer, {src: "./assets/0.png", alt: "Gallows"});
+  const gallowsContainer = renderElement(
+    "div",
+    "gallows-container",
+    gameContainer
+  );
+  gallowsImg = renderElement("img", null, gallowsContainer, {
+    src: "./assets/0.png",
+    alt: "Gallows",
+  });
   quizContainer = renderElement("div", "quiz", gameContainer);
   answerContainer = renderElement("p", "answer", quizContainer);
   questionContainer = renderElement("p", "question", quizContainer);
-  const guessesContainer = renderElement("p", "guesses", quizContainer, {innerText: "Incorrect guesses: "});
-  guessCounterContainer = renderElement("span", "guesses-counter", guessesContainer);
+  const guessesContainer = renderElement("p", "guesses", quizContainer, {
+    innerText: "Incorrect guesses: ",
+  });
+  guessCounterContainer = renderElement(
+    "span",
+    "guesses-counter",
+    guessesContainer
+  );
   guessesContainer.append("/6");
 
   quizContainer.append(virtualKeyboard);
 
-  answerPlaceholderRender(answerLength);
+  answerPlaceholderRender(question.answer);
   questionRender(question.question);
   guessesRender(0);
   gallowsRender(0);
@@ -96,32 +110,58 @@ const initialRender = () => {
   //       </div>
   //     </div>
   //   </div>`;
-
 };
 
-const answerPlaceholderRender = (answerLength) => {
-  const answerElement = document.querySelector('.answer');
-  placeholder = '';
+const answerPlaceholderRender = (answer, userAnswer) => {
+  let preparedAnswer = answer.toLowerCase();
+  userAnswer && (preparedUserAnswer = userAnswer.toLowerCase());
+  let answerLength = answer.length;
+  const answerElement = document.querySelector(".answer");
 
-  while(answerLength){
-    placeholder += '_';
-    answerLength--;
+  if (userAnswer) {
+    for (let i = 0; i < preparedAnswer.length; i++) {
+      if (preparedAnswer[i] === preparedUserAnswer) {
+        currentAnswerPlaceholder =
+          currentAnswerPlaceholder.substring(0, i) +
+          preparedUserAnswer +
+          currentAnswerPlaceholder.substring(i + 1);
+      }
+    }
+  } else {
+    while (answerLength) {
+      currentAnswerPlaceholder += "_";
+      answerLength--;
+    }
   }
-  answerContainer.innerHTML = placeholder;
-}
+  answerContainer.innerHTML = currentAnswerPlaceholder;
+};
 
 const questionRender = (question) => {
   questionContainer.innerHTML = question;
-}
+};
 
 const guessesRender = (guessesCounter) => {
   guessCounterContainer.innerHTML = `${guessesCounter}`;
-}
+};
 
 const gallowsRender = (guessesCounter) => {
   gallowsImg.src = `./assets/${guessesCounter}.png`;
-}
+};
 
 initialRender();
 
-const handleKeyPress = (letter) => {};
+const handleKeyPress = (letter) => {
+  if (currentQuestion.answer.toLowerCase().includes(letter)) {
+    answerPlaceholderRender(currentQuestion.answer, letter);
+  } else {
+    if(guessesCounter < 6){
+      guessesCounter += 1;
+      gallowsRender(guessesCounter);
+      guessesRender(guessesCounter);
+    }
+    if(guessesCounter === 6) {
+      gallowsRender(guessesCounter);
+      guessesRender(guessesCounter);
+    }
+  }
+};
